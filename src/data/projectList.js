28b65d7ab5d533efa537eef07f77b158c5bf9f8d
Liza Feng <liza.feng05@myhunter.cuny.edu>
2023-07-20 21:40:11 -0400

    2.1 Work experience card display done

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -2,28 +2,46 @@ const projectList = [
   {
     key: 1,
     projectName: "Animal Mafia",
+    solo: false,
+    teamSize: 5,
+    startDate: "",
+    endDate: "",
     technologies: [],
     responsibilities: [],
     problem: "",
-    results: "",
+    //0th index would have the approach description.
+    //1st index would have the results which includes links
+    results: ["", ["", ""]],
     visuals: "",
   },
   {
     key: 2,
     projectName: "",
+    solo: true,
+    teamSize: 1,
+    startDate: "",
+    endDate: "",
     technologies: [],
     responsibilities: [],
     problem: "",
-    results: "",
+    //0th index would have the approach description.
+    //1st index would have the results which includes links
+    results: ["", ["", ""]],
     visuals: "",
   },
   {
     key: 3,
     projectName: "",
+    solo: true,
+    teamSize: 1,
+    startDate: "",
+    endDate: "",
     technologies: [],
     responsibilities: [],
     problem: "",
-    results: "",
+    //0th index would have the approach description.
+    //1st index would have the results which includes links
+    results: ["", ["", ""]],
     visuals: "",
   },
 ];
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,36 @@
+import { workExp } from "../data/workExp";
+import "../css/Work.css";
+import Card from "react-bootstrap/Card";
+
 const Work = () => {
-  return <h1>Work</h1>;
+  return (
+    <div className="workContainer card-deck">
+      {workExp.map(function (exp, i) {
+        return (
+          <a
+            className="workCard"
+            href="https://stackoverflow.com/questions/70068954/make-entire-card-clickable-by-targeting-a-inside-of-it"
+          >
+            <Card className="mb-4 " style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>
+                  <h1>{exp.company}</h1>
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  <h5>{exp.title}</h5>
+                </Card.Subtitle>
+                <Card.Text className="text-muted">
+                  <h6>
+                    {exp.startDate} - {exp.endDate}
+                  </h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Work;
