aff1fb25f358ae09d6bbd33da27edbb570983925
Liza Feng <liza.feng05@myhunter.cuny.edu>
2023-08-16 22:51:54 -0400

    changing href after changing hosting from main branch instead of gh_pages

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -4,7 +4,7 @@ const Download = () => {
   };
   return (
     <div style={styled}>
-      <a href="/LizaResume.pdf" download>
+      <a href="LizaResume.pdf" download="LizaResume.pdf">
         <button className="btn btn-outline-primary btn-rounded">
           Download Resume
         </button>
