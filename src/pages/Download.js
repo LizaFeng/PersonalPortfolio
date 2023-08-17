6e9158703809122752a5c67ab888aafb844bd0ce
Liza Feng <liza.feng05@myhunter.cuny.edu>
2023-08-16 22:35:20 -0400

    changing href again

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -5,8 +5,8 @@ const Download = () => {
   return (
     <div style={styled}>
       <a
-        href="lizafeng.github.io/PersonalPortfolio/public/LizaResume.pdf"
-        download="lizafeng.github.io/PersonalPortfolio/public/LizaResume.pdf"
+        href="/PersonalPortfolio/public/LizaResume.pdf"
+        download="LizaResume.pdf"
       >
         <button className="btn btn-outline-primary btn-rounded">
           Download Resume
